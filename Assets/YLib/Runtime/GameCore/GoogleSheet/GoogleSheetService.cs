8119a252ec59f7be8944797afe566fea492fd213
ying722 <ying72.lai@gmail.com>
2024-12-02 12:33:16 +0800

    更新google sheet純獲取Json字串

--- Assets/YLib/Runtime/GameCore/GoogleSheet/GoogleSheetService.cs
+++ Assets/YLib/Runtime/GameCore/GoogleSheet/GoogleSheetService.cs
@@ -13,6 +13,9 @@ namespace YLib.GoogleSheet
         public static void LoadDataArray<T>(string url , Action<T[]> complete)
         {
         #if UNITY_EDITOR
+            /// <summary>
+            /// 獲取 JSON 並轉換為陣列的通用方法
+            /// </summary>
             EditorWebRequest.Complete += delegate(string jsonText)
             {
                 try
@@ -28,6 +31,27 @@ namespace YLib.GoogleSheet
             };
             EditorWebRequest.Request(url);
         #endif
+        }
+
+        /// <summary>
+        /// 獲取原始 JSON 字串
+        /// </summary>
+        public static void LoadJson(string url, Action<string> complete)
+        {
+            EditorWebRequest.Complete += delegate (string jsonText)
+            {
+                try
+                {
+                    // 直接返回 JSON 字串
+                    complete(jsonText);
+                }
+                catch (Exception)
+                {
+                    EditorWebRequest.ClearAction();
+                    throw;
+                }
+            };
+            EditorWebRequest.Request(url);
         }
 
     #endregion
