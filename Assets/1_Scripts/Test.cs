18ce46c7ba30b246dac3ef1e36ae8bebf99d42e6
ying722 <ying72.lai@gmail.com>
2024-12-22 20:27:36 +0800

    ResourceModule增加namespace

--- Assets/1_Scripts/Test.cs
+++ Assets/1_Scripts/Test.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using YLib.GameCore;
 
 public class Test : MonoBehaviour
 {
--- Assets/YLib/Runtime/GameCore/Module/Source/ResourceModule.cs
+++ Assets/YLib/Runtime/GameCore/Module/Source/ResourceModule.cs
@@ -1,45 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using YLib.GameCore;   
 using UnityEngine.AddressableAssets;
 using System.Threading.Tasks;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
-public class ResourceModule : BaseModule
+
+
+namespace YLib.GameCore
 {
-    /// <summary>
-    /// 獲取 Addressables 系統中的資源。
-    /// </summary>
-    /// <typeparam name="T">資源的類型</typeparam>
-    /// <param name="address">資源的 Addressables 地址</param>
-    /// <returns>返回資源對象</returns>
-    public async Task<T> GetResource<T>(string address) where T : Object
+    public class ResourceModule : BaseModule
     {
-        AsyncOperationHandle<T> handle = Addressables.LoadAssetAsync<T>(address);
-        await handle.Task;
-
-        if (handle.Status == AsyncOperationStatus.Succeeded)
+        /// <summary>
+        /// 獲取 Addressables 系統中的資源。
+        /// </summary>
+        /// <typeparam name="T">資源的類型</typeparam>
+        /// <param name="address">資源的 Addressables 地址</param>
+        /// <returns>返回資源對象</returns>
+        public async Task<T> GetResource<T>(string address) where T : Object
         {
-            Debug.Log($"Resource loaded successfully: {address}");
-            return handle.Result;
+            AsyncOperationHandle<T> handle = Addressables.LoadAssetAsync<T>(address);
+            await handle.Task;
+
+            if (handle.Status == AsyncOperationStatus.Succeeded)
+            {
+                Debug.Log($"Resource loaded successfully: {address}");
+                return handle.Result;
+            }
+            else
+            {
+                Debug.LogError($"Failed to load resource at address: {address}");
+                return null;
+            }
         }
-        else
+
+        public async Task<GameObject> Spawn(string address)
         {
-            Debug.LogError($"Failed to load resource at address: {address}");
-            return null;
+            GameObject obj = await GetResource<GameObject>(address);
+            
+            return GameobjectPool.Spawn(address,obj);
         }
-    }
 
-    public async Task<GameObject> Spawn(string address)
-    {
-        GameObject obj = await GetResource<GameObject>(address);
-        
-        return GameobjectPool.Spawn(address,obj);
-    }
-
-    public async Task DeSpawn(GameObject obj)
-    {
-        GameobjectPool.DeSpawn(obj);
+        public async Task DeSpawn(GameObject obj)
+        {
+            GameobjectPool.DeSpawn(obj);
+        }
     }
 }
